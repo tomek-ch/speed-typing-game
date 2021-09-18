d3db49e5049141213cee404d4efe7349c1241d19
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-09-18 20:05:17 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div v-if="state === 'start'">
+    <h1>How fast can you type?</h1>
+    <button @click="start">Start</button>
+  </div>
+  <div v-if="state === 'inProgress'">
+    <div>Type this:</div>
+    <h2>{{ sentence }}</h2>
+    <div>Here:</div>
+    <input v-model="input" ref="inputRef"/>
+    <br>
+    <button @click="start">Restart</button>
+  </div>
+  <div v-else-if="state === 'end'">
+    <h2>Your score was:</h2>
+    <h1>{{ wordsPerSec }} words per second!</h1>
+    <button @click="start">Play again</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { nextTick, ref, watchEffect } from 'vue';
+import sentences from './sentences.json';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  setup() {
+    const state = ref('start');
+    const input = ref('');
+    const sentence = ref(null);
+    const inputRef = ref(null);
+    const startTime = ref(null);
+    const wordsPerSec = ref(null);
+
+    watchEffect(() => {
+      if (input.value === sentence.value) {
+        state.value = 'end';
+        const endTime = Date.now() / 1000;
+        const wordCount = sentence.value.split(' ').length;
+        wordsPerSec.value = ((wordCount / (endTime - startTime.value))).toFixed(2);
+        input.value = '';
+      }
+    });
+
+    const start = async () => {
+      sentence.value = sentences[Math.floor(Math.random() * sentences.length)];
+      startTime.value = Date.now() / 1000;
+      state.value = 'inProgress';
+      nextTick(() => inputRef.value.focus());
+    };
+
+    return {
+      state,
+      sentence,
+      input,
+      start,
+      wordsPerSec,
+      inputRef
+    };
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: sans-serif;
 }
 </style>
